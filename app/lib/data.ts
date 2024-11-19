45e4a576136c65c6b89d7cb07f26d760e0784e87
Peter Stricker <pstric@gmail.com>
2024-11-20 00:42:48 +0100

    Add simple function fetchCities

---
+++ app/lib/data.ts
@@ -0,0 +1,43 @@
+import { cities } from "./placeholder-data";
+
+export function fetchAllInvoices() {
+    const invoices = [
+        {
+            id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+            date: '2024-11-16',
+            store: 'Netto',
+            amount: '200.00',
+        },
+        {
+            id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+            date: '2024-11-15',
+            store: 'Fakta',
+            amount: '150.00',
+        },
+        {
+            id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+            date: '2024-11-14',
+            store: 'Bilka',
+            amount: '125.00',
+        },
+    ];
+    return invoices;
+}
+
+export function fetchInvoiceById(id: string) {
+    console.log(id);
+    const invoices = fetchAllInvoices();
+    var result = invoices[0];
+    invoices.forEach((invoice) => {
+        if (invoice.id === id) {
+            console.log('found');
+            result = invoice;
+        }
+    });
+    console.log(result);
+    return result;
+}
+
+export function fetchCities() {
+    return cities;
+}
