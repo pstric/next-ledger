360e18292fe4a3cdcbc795d5c2e7a596eb68185c
Peter Stricker <pstric@gmail.com>
2024-11-20 00:30:42 +0100

    Add table of cities

--- app/dashboard/cities/page.tsx
+++ app/dashboard/cities/page.tsx
@@ -1,5 +1,30 @@
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCities } from "@/app/lib/data";
+
 export default function Page() {
+    const cities = fetchCities();
+
     return (
-        <p>Cities</p>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Cities</h1>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Zip</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cities?.map((city) => (
+                        <tr key={city.id}>
+                            <td>{city.zip}</td>
+                            <td>{city.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
