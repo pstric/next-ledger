cc0c5e04de01253d191e05ff038134ae9406b8b5
Peter Stricker <pstric@gmail.com>
2024-11-20 00:41:29 +0100

    Add buttons to view, edit and delete

--- app/dashboard/cities/page.tsx
+++ app/dashboard/cities/page.tsx
@@ -9,18 +9,24 @@ export default function Page() {
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${lusitana.className} text-2xl`}>Cities</h1>
             </div>
-            <table>
-                <thead>
+            <table className="table min-w-full">
+                <thead className="text-left">
                     <tr>
                         <th>Zip</th>
                         <th>Name</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {cities?.map((city) => (
-                        <tr key={city.id}>
+                        <tr key={city.id} className="w-full">
                             <td>{city.zip}</td>
                             <td>{city.name}</td>
+                            <td>View</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
                         </tr>
                     ))}
                 </tbody>
