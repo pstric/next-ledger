710529df9f03b2e209de05551d3f0a378b20680d
Peter Stricker <pstric@gmail.com>
2024-11-20 20:33:52 +0100

    Edit link switched to button component

--- app/dashboard/cities/page.tsx
+++ app/dashboard/cities/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCities } from "@/app/lib/data";
 import ViewCity from "@/app/ui/cities/buttons/view";
+import EditCity from "@/app/ui/cities/buttons/edit";
 
 export default function Page() {
     const cities = fetchCities();
@@ -28,7 +29,7 @@ export default function Page() {
                             <td>{city.zip}</td>
                             <td>{city.name}</td>
                             <td><ViewCity id={city.id} /></td>
-                            <td><Link href="/dashboard/cities"><PencilIcon className="w-5" /></Link></td>
+                            <td><EditCity id={city.id} /></td>
                             <td><Link href="/dashboard/cities"><TrashIcon className="w-5" /></Link></td>
                         </tr>
                     ))}
