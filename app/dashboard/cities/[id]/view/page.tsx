f7b5105a33186d70f3892efa01b3e98f16f8047b
Peter Stricker <pstric@gmail.com>
2024-11-20 15:35:54 +0100

    Display selected city

--- app/dashboard/cities/[id]/view/page.tsx
+++ app/dashboard/cities/[id]/view/page.tsx
@@ -1,5 +1,24 @@
-export default function Page() {
+import { fetchCityById } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+
+export default async function Page(props: { params: Promise<{ id: string }>}) {
+    const params = await props.params;
+    const id = params.id;
+    const city = fetchCityById(id);
+
+    if (!city) {
+        return null;
+    }
+    
     return (
-        <p>View City</p>
+        <div>
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>View City</h1>
+            </div>
+            <div>
+                <span>{city?.zip}</span>
+                <span>{city?.name}</span>
+            </div>
+        </div>
     );
 }
