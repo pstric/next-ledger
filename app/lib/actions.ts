46d718c32556d4cdeaae98d66e7aee1b86a393ae
Peter Stricker <pstric@gmail.com>
2024-11-21 15:07:33 +0100

    Simple delete action just throws error

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+export async function deleteCity(id: string) {
+    throw new Error('Failed to Delete City.');
+    
+/*     try {
+        await sql`DELETE FROM cities WHERE id = ${id}`;
+    } catch (error) {
+        return {
+            message: `Database Error: Failed to Delete City. ${error}`,
+        };
+    }
+    revalidatePath('/dashboard/cities');
+ */}
