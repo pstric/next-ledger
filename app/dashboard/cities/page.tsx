7332767f4037d5eac2ffca55f01de41a5e5096b3
Peter Stricker <pstric@gmail.com>
2024-11-21 15:17:57 +0100

    Move delete link to button component

--- app/dashboard/cities/page.tsx
+++ app/dashboard/cities/page.tsx
@@ -1,9 +1,8 @@
-import { EyeIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCities } from "@/app/lib/data";
 import ViewCity from "@/app/ui/cities/buttons/view";
 import EditCity from "@/app/ui/cities/buttons/edit";
+import { DeleteCity } from "@/app/ui/cities/buttons/delete";
 
 export default function Page() {
     const cities = fetchCities();
@@ -30,7 +29,7 @@ export default function Page() {
                             <td>{city.name}</td>
                             <td><ViewCity id={city.id} /></td>
                             <td><EditCity id={city.id} /></td>
-                            <td><Link href="/dashboard/cities"><TrashIcon className="w-5" /></Link></td>
+                            <td><DeleteCity id={city.id} /></td>
                         </tr>
                     ))}
                 </tbody>
