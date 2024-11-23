ce2e431b6425db9a6dd70d3afef1cccf718be3c4
Peter Stricker <pstric@gmail.com>
2024-11-23 22:27:46 +0100

    Delete Edit invoice page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchInvoiceById } from "@/app/lib/data";
-
-export default function Page(id: string) {
-    const invoice = fetchInvoiceById(id);
-
-    return (
-        <div>Edit invoice: {invoice.id}</div>
-    );
-}
